d7ca00a1454344435aee7610f5e4e14404caea28
Abdel-Rahman <soon-ab@hotmail.com>
2016-12-14 14:57:53 +0200

    Abdo Commit

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <map>
 
-#include <SOIL.h>
+#include <SOIL/SOIL.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <assimp/Importer.hpp>
@@ -10,7 +10,7 @@
 #include <assimp/postprocess.h>
 
 #include "Utilities/graphics_utils.h"
-#include "Utilities/fonts.h"
+//#include "Utilities/fonts.h"
 
 #include "Model/PenModel.h"
 #include "Models/GameScene.h"
@@ -24,9 +24,10 @@ Camera camera(glm::vec3(0.0f, 4.0f, 5.0f));
 
 const GLint gameWidth = 750;
 const GLint gameHeight = 800;
-GLint gameScore = 0;
+long score = 0;
 
 GLFWwindow* window;
+collisionStatus collisionState;
 
 int main(int argc, const char * argv[]) {
     
@@ -37,20 +38,20 @@ int main(int argc, const char * argv[]) {
     }
 
     // Load SHADERS
-    Shader sceneShader("/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/shader.vs", "/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/shader.frag");
-    Shader materialShader("/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/objshader.vs", "/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/objshader.frag");
-    Shader textureShader("/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/carshader.vs", "/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/carshader.frag");
-    Shader fontShader("/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/font.vs", "/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/font.frag");
+    Shader sceneShader("/home/abzo/workspace/workspace/JayWalking/Shaders/shader.vs", "/home/abzo/workspace/workspace/JayWalking/Shaders/shader.frag");
+    Shader materialShader("/home/abzo/workspace/workspace/JayWalking/Shaders/objshader.vs", "/home/abzo/workspace/workspace/JayWalking/Shaders/objshader.frag");
+    Shader textureShader("/home/abzo/workspace/workspace/JayWalking/Shaders/carshader.vs", "/home/abzo/workspace/workspace/JayWalking/Shaders/carshader.frag");
+   // Shader fontShader("/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/font.vs", "/Users/ibrahimradwan/Desktop/Graphics/CrossyRoad/opengl/opengl/Shaders/font.frag");
     
     
     // Load gamescene
     GameScene gameScene(sceneShader);
     // Load MODELS
-    Penguin penguin(materialShader, "/Users/ibrahimradwan/Desktop/penguin.dae", camera);
+    Penguin penguin(materialShader, "/home/abzo/workspace/workspace/JayWalking/ModelsFiles/penguin.dae", camera);
     penguin.setPenPosition(0, 1.4f, -3.3f); // Set initial posisiton
-    Car car(textureShader, "/Users/ibrahimradwan/Desktop/Small_car_obj/Small car.obj");
-    Car truck(textureShader, "/Users/ibrahimradwan/Desktop/cubus_deutz_rund/tlf16_rund.obj");
-    Coin coin(materialShader ,"/Users/ibrahimradwan/Desktop/coin/c.obj");
+    Car car(textureShader, "/home/abzo/workspace/workspace/JayWalking/ModelsFiles/Small_car_obj/Small car.obj");
+    Car truck(textureShader, "/home/abzo/workspace/workspace/JayWalking/ModelsFiles/cubus_deutz_rund/tlf16_rund.obj");
+    Coin coin(materialShader ,"/home/abzo/workspace/workspace/JayWalking/ModelsFiles/coin/c.obj");
     
     // Generate lanes
     vector<lane> lanesArray;
@@ -61,7 +62,7 @@ int main(int argc, const char * argv[]) {
     GLuint frameCount = 0;
    
     // Loading fonts
-    Fonts fonts(fontShader, gameWidth, gameHeight, "/Users/ibrahimradwan/Desktop/zorque.ttf");
+  //  Fonts fonts(fontShader, gameWidth, gameHeight, "/Users/ibrahimradwan/Desktop/zorque.ttf");
     
     // Load the lane-cube into the fragment
     GLuint VBO, VAO;
@@ -89,6 +90,9 @@ int main(int argc, const char * argv[]) {
         graphicsUtilities.do_movement(deltaTime, movingForward, movingBackward, movingRight, movingLeft);
         penguin.move(movingForward, movingBackward, movingRight, movingLeft,camera, deltaTime, newStart, lanesArray);
         // penguin animations
+        collisionState=penguin.detectCollision(lanesArray);
+        cout <<collisionState<<endl;
+
         penguin.draw(camera.GetViewMatrix(), glm::radians(camera.Zoom), (float)gameHeight/gameWidth, 0.1f, 1000.0f, frameCount, (movingForward || movingRight || movingLeft || movingBackward), deltaTime, lanesArray);
         
         // Draw the scene (lanes + cars)
@@ -102,12 +106,11 @@ int main(int argc, const char * argv[]) {
         if(frameCount >= penguin.getPenguinMode()->getModelAnimations()[0].second.size()){
             frameCount = 0;
         }
-        // Todo @Abdo (proper score counting)
-        if(frameCount % 60 == 0){
-            gameScore++;
-        }
+        penguin.score(lanesArray,score);
+
+
         // Display score
-        fonts.RenderText("Score: " + to_string(gameScore), 25.0f, gameHeight - 60.0f, 0.8f, glm::vec3(1, 1, 0));
+      //  fonts.RenderText("Score: " + to_string(gameScore), 25.0f, gameHeight - 60.0f, 0.8f, glm::vec3(1, 1, 0));
         
         glfwSwapBuffers(window);
     }
@@ -116,3 +119,4 @@ int main(int argc, const char * argv[]) {
     glfwTerminate();
     return 0;
 }
+
